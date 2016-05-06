7d20c1d050c48a9c9676b5b6ec98ccec7096df6c
Aaron Gomez <agomezs@outlook.com>
2016-05-06 14:21:11 -0600

    User service

--- src/js/helpers/ajax.js
+++ src/js/helpers/ajax.js
@@ -1,23 +1,45 @@
 (function (w) {
     'use strict';
     
-    /**
-     * 
-     **/
-    var getJson = function (url, callback) {
-        var xhr = new XMLHttpRequest();
+    // /**
+    //  * 
+    //  **/
+    // var getJson = function (url, callback) {
+    //     var xhr = new XMLHttpRequest();
         
-        xhr.open('GET', url, true);
-        //xhr.responseType = "";
+    //     xhr.open('GET', url, true);
+    //     //xhr.responseType = "";
         
-        xhr.onload = function () {
-            if(this.status >= 200 && this.status < 400) {
-                callback(this.response)
-            }
-        };
-        xhr.send();
+    //     xhr.onload = function () {
+    //         if(this.status >= 200 && this.status < 400) {
+    //             callback(this.response)
+    //         }
+    //     };
+    //     xhr.send();
+    // };
+    
+    // w.$.getJson = getJson;
+    
+    var ajax = function (url, method) {
+        method = method || 'GET';
+        return new Promise(function(resolve, reject) {
+            var xhr = new XMLHttpRequest();
+            xhr.open(method, url, true);
+            
+            xhr.onload = function () {
+                if(xhr.status >= 200 && xhr.status < 400) {
+                    resolve(JSON.parse(xhr.response));
+                } else {
+                    reject(console.error( req.statusText));
+                }
+            }; 
+            xhr.onerror = function () {
+              reject(console.error("Something went wrong..."));  
+            };
+            xhr.send();
+        });
     };
     
-    w.$.getJson = getJson;
+    w.$.getJson = ajax;
     
 })(window)
--- src/js/services/userService.js
+++ src/js/services/userService.js
@@ -1,20 +1,44 @@
 (function (w) {
+    'use strict';
     
-    var USERS_URL = 'https://jsonplaceholder.typicode.com/users';
-    var POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
+    var USERS_URL = 'http://jsonplaceholder.typicode.com/users';
+    var POSTS_URL = 'http://jsonplaceholder.typicode.com/posts';
     
-    function doSometing(response) {
-        console.log('res', response);
+    var users = [];
+    var posts = [];
+    var usersFormated = []
+    
+    function buildUsersList() {
+        users.forEach(function(user){
+            user.posts = posts.filter(function (post) {
+                return user.id === post.userId;
+            });
+        });
     }
     
-    var UserService = {
+    function retrievePosts() {
+        w.$.getJson(POSTS_URL)
+        .then(function (response) {
+            posts = response;
+            buildUsersList();
+        });
+    }
+    
+    function retrieveUsers(response) {
+        w.$.getJson(USERS_URL)
+        .then(function (response) {
+            users = response;
+            // TODO: make it pararell
+            retrievePosts();
+        });
+    }
+    
+    var userService = {
       getUsers: function () {
-          w.$.getJson(USERS_URL, doSometing);
+          return users;
       }
     };
     
-    UserService.getUsers();
-    
-    w.$.prototype.userService = new UserService;
+    w.$.userService = userService;
     
 })(window)
