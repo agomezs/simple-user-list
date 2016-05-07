317d465abc82d3054792dfc12cf5daff7fb585fa
Aaron Gomez <agomezs@outlook.com>
2016-05-06 21:24:28 -0600

    UserDetail with working return button

--- src/js/components/userDetail.js
+++ src/js/components/userDetail.js
@@ -1,31 +1,41 @@
 (function(w, d) {
   'use strict';
   
-  var userDetailPage = function () {
+  var POST_DETAIL = '<div>{POST-DETAIL}</div>';
   
-    var POST_DETAIL = '<div>{POST-DETAIL}</div>';
+  function findUserById () {
+    var userId = decodeURI(window.location.hash);
+    userId = userId.split('/')[1];
+    w.$.userService.find(userId, setUserDetail);
+  }
+  
+  function cleanContent() {
+    d.querySelector('.details.page .post-details').innerHTML = '';
+  }
+  
+  function setUserDetail (user) {
+    w.$.overlay.disable();
+    var userEl = d.querySelector('.details.page .user-name');
+    userEl.innerHTML = user.name + ' ('+ user.username +')';
     
-    // Init functions
+    var postEl = d.querySelector('.details.page .post-details');
+    
+    user.posts.forEach(function(post) {
+      var postDetail = POST_DETAIL.replace('{POST-DETAIL}', post.id + '. ' + post.title);
+      postEl.innerHTML = postEl.innerHTML + postDetail;
+    });
+  };
+  
+  // Add Event Listener.
+  d.querySelector('.details.page #return-btn')
+    .addEventListener('click', function (){
+      w.location.hash = '#';
+    });;
+  
+  var userDetailPage = function () {
+    // Init function
+    cleanContent();
     findUserById();
-    
-    function findUserById () {
-      var userId = decodeURI(window.location.hash);
-      userId = userId.split('/')[1];
-      w.$.userService.find(userId, setUserDetail);
-    }
-    
-    function setUserDetail (user) {
-      w.$.overlay.disable();
-      var userEl = d.querySelector('.details.page .user-name');
-      userEl.innerHTML = user.name + ' ('+ user.username +')';
-      
-      var postEl = d.querySelector('.details.page .post-details');
-      
-      user.posts.forEach(function(post) {
-        var postDetail = POST_DETAIL.replace('{POST-DETAIL}', post.id + '. ' + post.title);
-        postEl.innerHTML = postEl.innerHTML + postDetail;
-      });
-    };
   };
   
   w.$.pages.userDetailPage = userDetailPage;
